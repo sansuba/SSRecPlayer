6ef9ba81d891acd59ba38951b113fd8d6dbab841
Subhash Sanjeewa <sanjsuba@gmail.com>
2015-11-12 11:34:27 +0530

    Initial Commit

---
+++ SSRP/SSRP.h
@@ -0,0 +1,19 @@
+//
+//  SSRP.h
+//  SSRP
+//
+//  Created by Subhash Sanjeewa on 11/12/15.
+//  Copyright © 2015 Persystance Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSRP.
+FOUNDATION_EXPORT double SSRPVersionNumber;
+
+//! Project version string for SSRP.
+FOUNDATION_EXPORT const unsigned char SSRPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSRP/PublicHeader.h>
+
+
